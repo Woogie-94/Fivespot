72445832465bf1ebac79b460035a6687751d0fa7
Woogie-94 <keu1106@naver.com>
2021-09-16 14:55:50 +0900

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { Route, Switch } from "react-router-dom";
 import React from "react";
+import Login from "./page/Login";
+import SignUp from "./page/SignUp";
+import Profile from "./page/Profile";
+import ArticleList from "./page/ArticleList";
+import ArticleDetail from "./page/ArticleDetail";
 
 const App = (): JSX.Element => {
-  return <></>;
+  return (
+    <>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/articles" component={ArticleList} />
+        <Route path="/articles/:slug" component={ArticleDetail} />
+      </Switch>
+    </>
+  );
 };
 
 export default App;
