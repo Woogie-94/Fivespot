44baee9ce75b2a11ff0d3ad5f231c61e639c64b7
Woogie-94 <keu1106@naver.com>
2021-09-16 15:08:52 +0900

    Add router Redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import React from "react";
 import Login from "./page/Login";
 import SignUp from "./page/SignUp";
@@ -9,13 +9,16 @@ import ArticleDetail from "./page/ArticleDetail";
 const App = (): JSX.Element => {
   return (
     <>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={SignUp} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/articles" component={ArticleList} />
-        <Route path="/articles/:slug" component={ArticleDetail} />
-      </Switch>
+      <BrowserRouter>
+        <Switch>
+          <Redirect exact path="/" to="/articles" />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/profile" component={Profile} />
+          <Route exact path="/articles" component={ArticleList} />
+          <Route path="/articles/:slug" component={ArticleDetail} />
+        </Switch>
+      </BrowserRouter>
     </>
   );
 };
