4acc5eaf5afa193a613a46804a7acecdcec72cda
Woogie-94 <keu1106@naver.com>
2021-09-17 19:46:39 +0900

    Add articlesReducer

---
+++ src/reducer/articlesReducer.ts
@@ -0,0 +1,40 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { RootStateOrAny } from "react-redux";
+import { ArticlesReducerState, GetArticlesBody } from "../types";
+
+export const getArticles = createAsyncThunk("articels", async (body: GetArticlesBody) => {
+  const response = await axios.get(`https://test.fivespot.space/api/articles`, { params: body });
+
+  return response.data;
+});
+
+const initialState: ArticlesReducerState = {
+  state: "pending",
+  error: "",
+  articles: [],
+};
+
+export const articlesReducer = createSlice({
+  name: "articles",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getArticles.pending, (state, action) => {
+        console.log(action, "p");
+        state.state = "pending";
+      })
+      .addCase(getArticles.fulfilled, (state, action) => {
+        state.state = "success";
+        state.articles = action.payload.articles;
+        console.log(action, "f");
+      })
+      .addCase(getArticles.rejected, (state, action) => {
+        state.state = "failed";
+        if (action.error.stack) state.error = action.error.stack;
+      });
+  },
+});
+
+export const articelsSelector = (state: RootStateOrAny): ArticlesReducerState => state.articlesReducer;
--- src/reducer/store.ts
+++ src/reducer/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { signUpReducer } from "../reducer/signUpReducer";
+import { articlesReducer } from "./articlesReducer";
 import { loginReducer } from "./loginReducer";
 
 export default configureStore({
   reducer: {
     signUpReducer: signUpReducer.reducer,
     loginReducer: loginReducer.reducer,
+    articlesReducer: articlesReducer.reducer,
   },
 });
