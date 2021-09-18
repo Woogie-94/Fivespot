d72e9a4d3a8498828106f07074a271626924381f
Woogie-94 <keu1106@naver.com>
2021-09-18 21:13:29 +0900

    Add Hooks useUserInfo

--- src/hooks/useUserInfo.ts
+++ src/hooks/useUserInfo.ts
@@ -1,5 +1,24 @@
-export const userUserInfo = (): string[] => {
-  const { email, username, token } = JSON.parse(localStorage.getItem("user")!);
+import { useCallback, useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { logOut } from "../reducer/loginReducer";
+import { UserInfo } from "../types";
 
-  return [email, username, token];
+export const useUserInfo = (): [UserInfo, () => void] => {
+  const [userInfo, setUserInfo] = useState<UserInfo>({ state: false, email: "", username: "", token: "" });
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (localStorage.getItem("user")) {
+      const { email, username, token } = JSON.parse(localStorage.getItem("user")!);
+      setUserInfo({ state: true, email, username, token });
+    }
+  }, []);
+
+  const onLogOut = useCallback((): void => {
+    localStorage.removeItem("user");
+    setUserInfo({ ...userInfo, state: false });
+    dispatch(logOut());
+  }, [userInfo, dispatch]);
+
+  return [userInfo, onLogOut];
 };
