b27285ec18a6b275374cca9ddeb796201fc741ac
Woogie-94 <keu1106@naver.com>
2021-09-18 21:13:04 +0900

    Add article & userinfo

--- src/types/index.ts
+++ src/types/index.ts
@@ -28,3 +28,45 @@ export interface LoginReducerState {
   state: string;
   error: string;
 }
+
+export interface GetArticlesBody {
+  tag?: string;
+  author?: string;
+  favorited?: string;
+  limit: number;
+  offset: number;
+}
+
+export interface Author {
+  username: string;
+  bio: string;
+  image: string | null;
+  following: boolean;
+}
+
+export interface Article {
+  createdAt: string;
+  updatedAt: string;
+  id: number;
+  slug: string;
+  title: string;
+  description: string;
+  body: string;
+  tagList: string[];
+  author: Author;
+  favorited: boolean;
+  favoritesCount: number;
+}
+
+export interface ArticlesReducerState {
+  state: string;
+  articles: Article[];
+  error: string;
+}
+
+export interface UserInfo {
+  state: boolean;
+  email: string;
+  username: string;
+  token: string;
+}
