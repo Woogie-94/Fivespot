f4f87e25410c28ba3088a144c09f560700f3a962
Woogie-94 <keu1106@naver.com>
2021-09-19 11:50:09 +0900

    Add controller & grid feature

--- src/page/Articles.tsx
+++ src/page/Articles.tsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import ArticlesController from "../components/ArticlesController";
+import Grid from "../components/Grid";
 import Header from "../components/Header";
+import Lists from "../components/Lists";
 
-const ArticleList = (): JSX.Element => {
+const Articles = (): JSX.Element => {
+  const [toggle, setToggle] = useState<string>("list");
+
   return (
     <>
       <Header />
+      <ArticlesContainer>
+        <ArticlesController toggle={toggle} setToggle={setToggle} />
+        {toggle === "list" ? <Lists /> : <Grid />}
+      </ArticlesContainer>
     </>
   );
 };
 
-export default ArticleList;
+const ArticlesContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  width: 100%;
+  min-height: calc(100vh - 80px);
+  background-color: #f6f6f8;
+`;
+
+export default Articles;
