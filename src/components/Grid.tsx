6bf19622434b47933c921cb432f9ff61693ca82b
Woogie-94 <keu1106@naver.com>
2021-09-19 11:47:04 +0900

    Add Grid component

---
+++ src/components/Grid.tsx
@@ -0,0 +1,80 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
+import { articelsSelector, getArticles } from "../reducer/articlesReducer";
+import { REQUEST_STATES, Article, GetArticlesBody } from "../types";
+import GridCard from "./GridCard";
+
+const Grid = (): JSX.Element => {
+  const [articles, setArticles] = useState<Article[]>([]);
+  const [articlesBody, setArticlesBody] = useState<GetArticlesBody>({ limit: 12, offset: 0 });
+  const [throttleWaiting, setThrottleWaiting] = useState<boolean>(false);
+
+  const dispatch = useDispatch();
+  const articleData = useSelector(articelsSelector);
+
+  const throttle = useCallback(
+    (callback: () => void, delay: number) => {
+      if (throttleWaiting) return;
+      if (!throttleWaiting) {
+        setThrottleWaiting(true);
+
+        setTimeout(() => {
+          callback();
+          setThrottleWaiting(false);
+        }, delay);
+      }
+    },
+    [throttleWaiting]
+  );
+
+  const onScroll = useCallback((): void => {
+    const { innerHeight } = window;
+    const { scrollHeight } = document.body;
+    const { scrollTop } = document.documentElement;
+
+    if (Math.round(scrollTop + innerHeight) >= scrollHeight) {
+      throttle(() => {
+        setArticlesBody((body) => {
+          return { ...body, offset: body.offset + 12 };
+        });
+      }, 300);
+    }
+  }, [throttle]);
+
+  useEffect(() => {
+    if (articlesBody.offset > 24) return;
+
+    dispatch(getArticles(articlesBody));
+  }, [dispatch, articlesBody]);
+
+  useEffect(() => {
+    if (articleData.state === REQUEST_STATES.SUCCESS) setArticles((articles) => [...articles, ...articleData.articles]);
+    if (articleData.state === REQUEST_STATES.FAILED) console.error(articleData.error);
+  }, [articleData]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.addEventListener("scroll", onScroll);
+  }, [onScroll, throttle]);
+
+  useEffect(() => console.log(articles), [articles]);
+
+  return (
+    <GridContainer>
+      {articles.map((article, index) => (
+        <GridCard key={index} article={article} />
+      ))}
+    </GridContainer>
+  );
+};
+
+const GridContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 1100px;
+  margin-top: 40px;
+`;
+
+export default Grid;
---
+++ src/components/GridCard.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import styled from "styled-components";
+import basicImg from "../images/basic_img_1.jpg";
+import { Article } from "../types";
+
+const GridCard = ({ article }: { article: Article }): JSX.Element => {
+  return (
+    <CardContainer>
+      <CardImgContainer>
+        <CardImg src={basicImg} alt="Card Img" />
+      </CardImgContainer>
+      <CardInfoContainer>
+        <CardTitle>{article.title}</CardTitle>
+        <CardDesc>{article.description}</CardDesc>
+        <CardAuthor>{article.author.username}</CardAuthor>
+      </CardInfoContainer>
+    </CardContainer>
+  );
+};
+
+const CardContainer = styled.div`
+  cursor: pointer;
+  transition: all 0.3s;
+  width: calc((100% - 80px) / 4);
+  margin: 0 10px 20px;
+  background-color: #fff;
+  box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.1);
+
+  &:hover {
+    transform: translateY(-10px);
+  }
+
+  @media screen and (max-width: 1099px) {
+    width: calc((100% - 80px) / 3);
+  }
+
+  @media screen and (max-width: 799px) {
+    width: calc((100% - 80px) / 2);
+  }
+`;
+
+const CardImgContainer = styled.div`
+  overflow: hidden;
+  position: relative;
+  width: 100%;
+  height: 150px;
+`;
+const CardImg = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+`;
+
+const CardInfoContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 150px;
+  padding: 10px;
+`;
+const CardTitle = styled.p`
+  width: 100%;
+  font-family: "NanumSquareEB";
+`;
+const CardDesc = styled.p`
+  width: 100%;
+  margin-top: 10px;
+  font-family: "NanumSquareR";
+  font-size: 14px;
+`;
+const CardAuthor = styled.p`
+  position: absolute;
+  bottom: 10px;
+  width: 100%;
+  margin-top: 10px;
+  font-family: "NanumSquareR";
+  color: #aaa;
+  font-size: 14px;
+`;
+
+export default GridCard;
